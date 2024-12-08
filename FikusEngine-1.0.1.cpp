243b9829cb225e337d372a55ffe041376f13f407
marek <marek@MAREK>
2024-12-08 14:56:59 +0100

    Přidejte soubory projektu.

---
+++ FikusEngine-1.0.1.cpp
@@ -0,0 +1,12 @@
+﻿// FikusEngine-1.0.1.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "FikusEngine-1.0.1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ FikusEngine-1.0.1.h
@@ -0,0 +1,7 @@
+﻿// FikusEngine-1.0.1.h: Soubor k zahrnutí pro standardní systémové soubory k zahrnutí
+// nebo soubory k zahrnutí specifické pro projekt.
+
+#pragma once
+
+#include <iostream>
+
